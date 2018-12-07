da6ebf57a73a604eb93434bfbb3a6f7ec45ffaf4
yaofang <yaof146@163.com>
2018-12-07 21:20:26 +0800

    content

--- src/components/customer.vue
+++ src/components/customer.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="customer">
-    aaa
+    <div>
+      <span>进行中</span>
+      <span>无效</span>
+    </div>
+    <!-- <router-link :to="{path:'./children/message'}"></router-link> -->
   </div>
+  
 </template>
 
 <script>
@@ -19,5 +24,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.customer div{
+  display:flex;
+  justify-content: space-between;
+}
+span{
+  border-radius: 5px;
+  color:#f00;
+  background:#fff;
+  border:1px solid #f00;
+}
+span:first-child{
+  color:#fff;
+  background:#f00;
+}
 </style>
--- src/pages/Firstpage.vue
+++ src/pages/Firstpage.vue
@@ -14,6 +14,7 @@
 
 <script>
 import first from '@/components/first'
+import customer from '@/components/customer'
 export default {
   name: 'Firstpage',
   data () {
